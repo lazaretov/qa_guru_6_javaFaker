26750463a71cce1af37ddc23d856db92b6e7f694
Timofei <lazaretov03@gmail.com>
2022-10-20 20:07:55 +0600

    add UserInfo data

---
+++ src/test/java/com/lazaretov/testData/UserInfo.java
@@ -0,0 +1,28 @@
+package com.lazaretov.testData;
+
+
+import com.github.javafaker.Faker;
+import com.lazaretov.utils.Month;
+
+import java.util.Locale;
+
+public class UserInfo {
+
+    private static final Faker faker = new Faker(new Locale("ru"));
+
+    public static String firstName = faker.name().firstName(),
+            lastName = faker.name().lastName(),
+            email = faker.internet().emailAddress(),
+            phone = faker.phoneNumber().subscriberNumber(10),
+            pic = "pic.JPG",
+            subject = "English",
+            hobby = "Reading",
+            state = "Haryana",
+            city = "Karnal",
+            address = faker.address().fullAddress(),
+            gender = faker.demographic().sex(),
+            day = String.valueOf(faker.number().numberBetween(1, 31)),
+            month = String.valueOf(Month.getRandomMonth()),
+            year = String.valueOf(faker.number().numberBetween(1950, 2010)),
+            date = day + " " + month + "," + year;
+}
